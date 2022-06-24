38ebd5aef897f182d28cf0f183ccf731b1ff5cc8
Anatoliy Aliaksandrau <a.aliaksandrau@elinext.com>
2022-06-24 20:03:35 +0300

    refactor: separate modules remove comments add constants and enums

--- src/constants/remoteControls.ts
+++ src/constants/remoteControls.ts
@@ -1,3 +1,14 @@
 export enum RemoteControls {
-    MOUSE_DOWN = 'mouse_down',
+  MOUSE_DOWN = "mouse_down",
+  MOUSE_UP = "mouse_up",
+  MOUSE_LEFT = "mouse_left",
+  MOUSE_RIGHT = "mouse_right",
+  MOUSE_POSITION = "mouse_position",
+  PRINT_SCREEN = "prnt_scrn",
+  DRAW_CIRCLE = "draw_circle",
+  DRAW_RECTANGLE = "draw_rectangle",
+  DRAW_SQUARE = "draw_square",
+  MOUSE_BUTTON_DOWN = "down",
+  MOUSE_BUTTON_UP = "up",
+  UNKNOWN_COMMAND = "UNKNOWN COMMAND READ TASK DESCRIPTION !!!",
 }
